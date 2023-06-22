7fc93faaaf8d4845d00610a0345ef77fe4cdbea0
fgradbsdsw <137376962+fgradbsdsw@users.noreply.github.com>
2023-06-22 12:49:41 +0700

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ci9tuulgkuvusatut760-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "NkS8qw5948KGDtwqT6EgnuvDCWtiFbwv",
+    DB: "frgdb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
